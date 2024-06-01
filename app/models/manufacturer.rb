9cca3bfedc6af465d8b7243779abc0b042f73aff
Vince Carollo <vcarollo@g2.com>
2024-06-01 15:48:04 -0500

    Add manufacturer model specs

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,4 +1,5 @@
 class Manufacturer < ApplicationRecord
+  has_many :discs
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
 end
--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Manufacturer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Manufacturer.create!(name: 'Innova', slug: 'innova')
+  end
+
+  it { should have_many(:discs) }
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:slug) }
+  it { should validate_uniqueness_of(:slug) }
 end
