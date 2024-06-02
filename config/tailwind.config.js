6afec283d19abaa27c1037a2bfaea7f6f891fa6e
Vince Carollo <vcarollo@g2.com>
2024-06-02 09:14:27 -0500

    feat: Set up brand basics

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'poly-green': '#004F2D',
+        'sea-green': '#0A8754',
+        'air-force-blue': '#508CA4',
+        'cadet-gray': '#91AEC1',
+        'columbia-blue': '#BFD7EA'
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
