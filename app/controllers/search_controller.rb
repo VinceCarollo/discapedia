eb86eb4a9571ff94e01418f3d2e2e7f032148d94
Vince Carollo <vcarollo@g2.com>
2024-06-03 14:09:31 -0500

    feat: Set up pg_search with some styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @search_results = Disc.all
-
+    @search_results = Disc.includes(:manufacturer)
+                          .search_by_name(params[:q])
+                          .order('manufacturers.name ASC, discs.name ASC')
+                          .group_by(&:manufacturer)
     render layout: false
   end
 end
--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,4 +1,8 @@
 class Disc < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name, against: :name, using: { tsearch: { prefix: true } }
+
   belongs_to :manufacturer
   validates :name, presence: true, uniqueness: { scope: :manufacturer_id }
   validates :slug, presence: true, uniqueness: { scope: :manufacturer_id }
