c321ed1b37d22e1700aa659d12a44d5c843bacb5
Vince Carollo <vcarollo@g2.com>
2024-06-19 21:58:24 -0500

    feat: Allow searching for manufacturer names

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
-    @search_results = Disc.includes(:manufacturer)
-                          .search_by_name(params[:q])
-                          .order('manufacturers.name ASC, discs.name ASC')
+    @search_results = Disc.search_by_name(params[:q])
+                          .order(:manufacturer_id)
                           .group_by(&:manufacturer)
+
     render layout: false
   end
 end
--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,12 +1,18 @@
 class Disc < ApplicationRecord
   include PgSearch::Model
 
-  pg_search_scope :search_by_name, against: :name, using: { tsearch: { prefix: true } }
+  pg_search_scope :search_by_name,
+                  associated_against: { manufacturer: :name },
+                  against: :name,
+                  using: { tsearch: { prefix: true } }
 
   has_one_attached :image
 
   belongs_to :manufacturer
-  validates :buy_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true
+
+  validates :buy_url,
+            format: URI::DEFAULT_PARSER.make_regexp(%w[http https]),
+            allow_blank: true
   validates :name, presence: true, uniqueness: { scope: :manufacturer_id }
   validates :slug, presence: true, uniqueness: { scope: :manufacturer_id }
   validates_presence_of :speed,
