d883158b6310d8846ce92dbeefe1e8e95cf17ffc
Vince Carollo <vcarollo@g2.com>
2024-06-20 21:55:17 -0500

    fix: Remove unnecessary order from disc query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     @search_results = Disc.search_by_name(params[:q])
-                          .order(:manufacturer_id)
                           .group_by(&:manufacturer)
 
     render layout: false
