c99968310e76ff2d77bda6694a4184cea390878b
Vince Carollo <vcarollo@g2.com>
2024-06-27 21:17:12 -0500

    fix: Eager load manufacturer for group_by

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @search_results = Disc.search_by_name(params[:q])
+                          .includes(:manufacturer)
                           .group_by(&:manufacturer)
 
     render layout: false
