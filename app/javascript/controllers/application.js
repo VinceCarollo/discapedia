f91627cff2c93c098174ab276d749c34b57febc5
Vince Carollo <vcarollo@g2.com>
2024-06-29 17:07:35 -0500

    chore: Remove stimulus-autocomplete and set up search form

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import { Autocomplete } from 'stimulus-autocomplete'
 
 const application = Application.start()
-
-application.register('autocomplete', Autocomplete)
 
 // Configure Stimulus development experience
 application.debug = false
