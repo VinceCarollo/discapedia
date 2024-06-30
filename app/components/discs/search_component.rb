4d36d3eabae0a27b899b3df2023d1ee466163b4d
Vince Carollo <vcarollo@g2.com>
2024-06-30 16:44:17 -0500

    feat: Add auto submit delay

--- app/components/discs/search_component.rb
+++ app/components/discs/search_component.rb
@@ -1,4 +1,20 @@
 module Discs
   class SearchComponent < ViewComponent::Base
+    AUTO_SUBMIT_DELAY = 200
+
+    private_constant :AUTO_SUBMIT_DELAY
+
+    private
+
+    def auto_submit_delay_value
+      200
+    end
+
+    def input_data_actions
+      %w[input->reveal#show
+         input->auto-submit#submit
+         input->disc-search#hideResultsOnClear
+         focus->disc-search#showResultsIfPresent].join(' ')
+    end
   end
 end
