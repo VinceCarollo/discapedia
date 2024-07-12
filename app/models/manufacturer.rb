8315eed6abe94d79e0483708a66bd7aaa72d0182
Vince Carollo <vcarollo@g2.com>
2024-07-11 23:09:35 -0500

    feat: Ensure manufacturer names are saved as capitlized

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,4 +1,6 @@
 class Manufacturer < ApplicationRecord
+  include Capitalizable
+
   has_many :discs
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
